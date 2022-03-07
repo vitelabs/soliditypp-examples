53165125016eb44508ade267e41697f0915a8627
charles-liu <charles@vite.org>
2022-03-08 03:35:09 +0800

    add callback example using function type

---
+++ test/callback.spec.ts
@@ -0,0 +1,44 @@
+import { describe } from "mocha";
+import { expect } from "chai";
+const vite = require('@vite/vuilder');
+import config from "./vite.config.json";
+
+let provider: any;
+let deployer: any;
+
+describe('test callback', () => {
+  before(async function() {
+    provider = vite.localProvider();
+    deployer = vite.newAccount(config.networks.local.mnemonic, 0);
+  });
+
+  it('test callback', async () => {
+    // compile
+    const compiledContracts = await vite.compile('callback.solpp');
+    expect(compiledContracts).to.have.property('A');
+    expect(compiledContracts).to.have.property('B');
+
+    // deploy A
+    const A = compiledContracts.A;
+    A.setDeployer(deployer).setProvider(provider);
+    await A.deploy({});
+    expect(A.address).to.be.a('string');
+
+    // deploy B
+    const B = compiledContracts.B;
+    B.setDeployer(deployer).setProvider(provider);
+    await B.deploy({
+      params: [A.address!]
+    });
+    expect(B.address).to.be.a('string');
+    console.log(B.address)
+
+    // call B.test();
+    await B.call('test', [], {});
+
+    await B.waitForHeight(3);
+
+    // check B.data
+    expect(await B.query('data', [])).to.be.deep.equal(['3']);
+  });
+});
